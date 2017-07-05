443f38450861a1b850f210dad769e5ef4e354b7e
Adam Setapen <adam@mayfieldrobotics.com>
2017-07-05 12:58:26 -0700

    fix weird depends

--- helpers/build_debian.sh
+++ helpers/build_debian.sh
@@ -6,7 +6,7 @@ fpm \
   --architecture native \
   --python-install-bin "/usr/bin" \
   --python-install-lib "/usr/lib/python2.7/dist-packages" \
-  --depends "requests, six python-mayfield-tools" \
+  --depends "python-requests, python-six, python-mayfield-tools" \
   --vendor "Mayfield Robotics" \
   --maintainer "Adam Setapen <adam@mayfieldrobotics.com>" \
   setup.py
